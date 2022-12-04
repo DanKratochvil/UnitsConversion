a53a197d42edf3791c29de85ef4bcfa64f48464e
DanKratochvil <64211031+DanKratochvil@users.noreply.github.com>
2022-12-04 12:21:56 +0100

    Add files via upload

---
+++ UnitsConversionLib/ExtensionMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitsConversion;
+
+namespace UnitsConversion
+{
+    public static class MyExtensions
+    {      
+        internal static bool HasValue(this (Unit, Unit) tuple)
+        {
+            return tuple != (Unit.none, Unit.none);
+        }
+    }
+}
